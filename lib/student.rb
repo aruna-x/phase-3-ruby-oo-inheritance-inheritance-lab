5646a5532404f210e6f35af1940a756afb39e89b
Aruna <aruna.evan15@gmail.com>
2021-11-08 09:23:14 -0800

    commit tree 776b67394beec3ed94c57d7538acce0dc6c1157a

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(k)
+        @knowledge << k
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
